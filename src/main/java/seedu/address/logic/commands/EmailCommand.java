09c89ef634126d6e91cca4de80009027d2da1674
jin-ting <cheng-jt25@hotmail.com>
2017-11-06 21:32:19 +0800

    Remove unused import at emailCommand

--- src/main/java/seedu/address/logic/commands/EmailCommand.java
+++ src/main/java/seedu/address/logic/commands/EmailCommand.java
@@ -10,10 +10,8 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-import seedu.address.commons.core.EventsCenter;
 import seedu.address.commons.core.Messages;
 import seedu.address.commons.core.index.Index;
-import seedu.address.commons.events.ui.ShowEmailRequestEvent;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.person.ReadOnlyPerson;
 import seedu.address.model.person.email.Email;
